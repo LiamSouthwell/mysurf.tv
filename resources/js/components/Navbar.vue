9250c425756e3d84fc37dc28d1220a3284158337
Wasif <41457798+wchow3@users.noreply.github.com>
2019-04-06 19:12:47 +1000

    navbar with vid

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand">Surfing Australia</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Playlists</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Latest</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Trending</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Events</a>
+                </li>
+            </ul>
+            </div>
+            <button class="search">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+    </nav>
+</template>
