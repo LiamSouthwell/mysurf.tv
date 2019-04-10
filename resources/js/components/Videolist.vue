c0bfcf2d98450f05c8589aecee9536163af2a3cf
liamso <asleep.and.hairy@gmail.com>
2019-04-10 13:05:32 +1000

    Misc CSS fixed etc

--- resources/js/components/Videolist.vue
+++ resources/js/components/Videolist.vue
@@ -1,22 +1,26 @@
+
 <template>
     <div>
         <div v-for="(video, index) in videos">
 
             <div class="col myCol" style="position: relative; display: block; max-width: 960px;">
-                <div style="height: 200px; border: 1px solid black">
+                <div style="height: 200px; border: 1px solid black; border-bottom: none;" class="video">
                     <a href="" :id="'link'+index" style="width: inherit; height: inherit">
                         <img style="width: inherit; height: inherit" src="" :id="'thumbnail'+index"/>
                     </a>
-                    <div class="videoDetails" style="border: 1px solid black">
-                        <h3> Sample Name </h3>
-                        <h6> Video date | video duration </h6>
-                        <div class="addBtn">
-                            <button>
-                                <span class="fa fa-plus" aria-hidden="true"></span>
-                            </button>
+                    <div class="videoInfo">
+                        <div class="row">
+                            <div class="videoSub" :id="'title'+index"> Title </div>
+                            <hr>
+                        </div>
+                        <div class="row">
+                            <div class="videoSub half" :id="'date'+index"> Date</div>
+                            <div class="videoSub half text-right" :id="'duration'+index"> Duration </div>
+                            <div style="height: 0;">&nbsp;</div>
                         </div>
                     </div>
                 </div>
+
             </div>
 
         </div>
@@ -24,6 +28,38 @@
 </template>
 
 <style scoped>
+    hr{
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    .row{
+        width: 95%;
+        margin: auto;
+    }
+    .video{
+
+    }
+    .half{
+        width: 50%;
+        float: left;
+
+    }
+
+    .text-right{
+        text-align: right;
+    }
+
+    .videoSub{
+        padding: 0;
+        margin: 0;
+        font-size: 12px;
+    }
+    .videoInfo{
+        background-color: rgba(255,255,255,0.3);
+        width: inherit;
+        outline: 1px solid black;
+    }
+
     div {
         width: 100%;
         margin-left: 0;
@@ -32,7 +68,7 @@
 
     .myCol {
         padding-bottom: 10px;
-        margin-bottom: 100px;
+        margin-bottom: 55px;
     }
 
     .addBtn {
@@ -69,6 +105,9 @@
                 response.data.forEach(function(videoInfo, index){
                     document.getElementById("thumbnail"+index).src = videoInfo.poster;
                     document.getElementById("link"+index).href = 'watch/'+videoInfo.id;
+                    document.getElementById("title"+index).innerHTML = videoInfo.name;
+                    document.getElementById("date"+index).innerHTML = videoInfo.created_at.substring(0, 10);
+                    document.getElementById("duration"+index).innerHTML = videoInfo.duration;
                     //WASIF - there is a lot of info for the video accessible here (title, playtime, tags etc), when fields are available as an overlay I will fill them out. Use console.log(videoInfo) to see what is available. Currently thumbnails also link to a separate page - this will change to single page app standards // 
                 });
             }));
